dd3100fcee77a49e6385dd4936168b15e1492f36
Abdullah-codes <abdullahshaikhneo@gmail.com>
2021-09-08 17:39:25 +0530

    comment out scripts

--- bash-script-login.sh
+++ bash-script-login.sh
@@ -4,8 +4,8 @@ echo "hi from bash script"
 
 echo $SSH_KEY
 
-# echo "copying flask app"
-# scp -r -i $SSH_KEY ./flask-app ubuntu@52.66.164.150:/home/ubuntu
+echo "copying flask app"
+scp -r -i $SSH_KEY ./flask-app ubuntu@52.66.164.150:/home/ubuntu
 
 ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@52.66.164.150 'bash -s' < ./bash-script-automation.sh
 
