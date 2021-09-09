9286097bdfcb3aa47312b7dd485b3576832a6035
Abdullah-codes <abdullahshaikhneo@gmail.com>
2021-09-09 18:10:39 +0530

    changing ip in bashscript 2

--- bash-script-login.sh
+++ bash-script-login.sh
@@ -5,11 +5,11 @@ echo "hi from bash script"
 echo $SSH_KEY
 
 echo "first ssh attempt"
-ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@13.127.104.73
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@15.206.82.83
 
 echo "copying flask app"
-scp -r -i $SSH_KEY ./flask-app ubuntu@13.127.104.73:/home/ubuntu
+scp -r -i $SSH_KEY ./flask-app ubuntu@15.206.82.83:/home/ubuntu
 
-#ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@13.127.104.73 'bash -s' < ./bash-script-automation.sh
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@15.206.82.83 'bash -s' < ./bash-script-automation.sh
 
 #scp -i $SSH_KEY ./bash-script-automation.sh ubuntu@52.66.164.150:/home/ubuntu
